70de37773f0427cd64e07f85ecaebd79810d0b64
ra2457011 <otavio.ninin@unesp.br>
2026-05-09 12:10:36 -0300

    cocommit

--- database/migrations/2026_05_02_132521_create_cursos_table.php
+++ database/migrations/2026_05_02_132521_create_cursos_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('cursos', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('titulo');
             $table->string('descricao');
             $table->string('imagem');
